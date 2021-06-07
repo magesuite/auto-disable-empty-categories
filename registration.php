97e0b0d6277f9843773ac5ca2aeb428374adc1e4
Nick Loiko <nick@devbook.local>
2021-06-07 16:12:33 +0300

    module init

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageSuite_AutoDisableEmptyCategories',
+    __DIR__
+);
